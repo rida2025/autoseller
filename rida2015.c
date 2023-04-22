4063ff6fde3983ac4f2a4dbdd56a29b1a8f5429c
rida2015 <90518506+rida2025@users.noreply.github.com>
2023-04-22 14:28:29 +0000

    auto clicker
    it use f6 to run the code each time f6 is clicked and f12 to exit the programme

---
+++ rida2015.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+void click() 
+{
+    INPUT input = {0};
+
+    input.type = INPUT_MOUSE;
+    input.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+    SendInput(1, &input, sizeof(INPUT));
+
+    input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+int main() 
+{
+    int RandomList[] = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
+    int len = sizeof(RandomList) / sizeof(RandomList[0]);
+    srand(time(NULL));
+    printf("the auto seller is ON\n");
+    while(1){
+    	if(GetAsyncKeyState(VK_F6))
+    	{
+
+		    int x = 1372;
+		    int y = 642;
+
+		    for (int i = 0; i < 4; ++i)
+		    {
+		    	for (int i = 0; i < 7; ++i)
+		    	{
+		    		int rx = RandomList[rand() % len];
+		    		int ry = RandomList[rand() % len];
+		    		SetCursorPos(x+rx, y+ry);
+		    		printf("we clicked on x: %d y: %d\n", x+rx, y+ry);
+		    		x = x + 80;
+		    		click();
+		    	}
+		    	x = 1372;
+		    	y = y + 80;
+		    }
+    	}
+    	else if (GetAsyncKeyState(VK_F12))
+    	{
+    		break;
+    	}
+    }
+    return 0;
+}
